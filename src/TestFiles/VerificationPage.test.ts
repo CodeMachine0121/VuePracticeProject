ccb9ad63622ebfbaab1f23ba2f00fab701ef4df3
james.hsueh77 <james.hsueh@titansoft.com.sg>
2023-10-20 23:23:27 +0800

    add test

--- src/TestFiles/VerificationPage.test.ts
+++ src/TestFiles/VerificationPage.test.ts
@@ -10,9 +10,20 @@ describe("otp page", async () => {
         test("should be 4 input boxs", () => {
             expect(inputboxs.length).toBe(4);
         });
-        test("should shift to next input box when input 1 digit", async () => {
-            await inputboxs[0].setValue("1");
+        test("should be able to enter digit", async () => {
+            await inputboxs[0].setValue('1')
             expect(inputboxs[0].element.value ).toBe('1');
+            await inputboxs[0].trigger('input')
+            expect(inputboxs[0].element.value ).toBe('1');
+            expect(inputboxs[1].element.value ).toBe('');
+        });
+        test("should be able to remove digit", async () => {
+            await inputboxs[0].setValue('1')
+            await inputboxs[1].setValue('2')
+            await inputboxs[1].trigger('keydown', {key: 'Backspace'});
+            await inputboxs[1].trigger('keydown', {key: 'Backspace'});
+            expect(inputboxs[0].element.value ).toBe('1');
+            expect(inputboxs[1].element.value ).toBe('');
         });
     });
 
